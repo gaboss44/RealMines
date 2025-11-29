67c38f32e906b54536a3d377c097d3aae2638e23
gaboss44 <cpr180821.gs@gmail.com>
2025-11-29 08:46:46 -0500

    Pass Cancellable Event to MineBlockBreakEvent

--- realmines-plugin/src/main/java/joserodpt/realmines/plugin/managers/MineManager.java
+++ realmines-plugin/src/main/java/joserodpt/realmines/plugin/managers/MineManager.java
@@ -357,7 +357,10 @@ public class MineManager extends MineManagerAPI {
                             if (mi.isBlockMiningDisabled()) {
                                 e.setCancelled(true);
                             } else {
-                                Bukkit.getPluginManager().callEvent(new MineBlockBreakEvent(p, mine, block, broken));
+                                Bukkit.getPluginManager().callEvent(new MineBlockBreakEvent(e, p, mine, block, broken));
+                                if (e.isCancelled()) {
+                                    return null;
+                                }
                                 return mine.getMineItems().get(block.getType());
                             }
                         }
